f4a0ea11a9752fa2ab996f9ef4254a4060e11b79
Parth Shandilya <parth.shandilya@cern.ch>
2021-12-16 15:45:00 +0100

    tests: add sleep in creation and deletion of records in e2e tests

--- tests/e2e/test_analysis_cli.py
+++ tests/e2e/test_analysis_cli.py
@@ -23,6 +23,7 @@
 # as an Intergovernmental Organization or submit itself to any jurisdiction.
 
 import json
+import time
 import pytest
 
 
@@ -61,7 +62,6 @@ def test_analysis_get_drafts_with_page_without_results(cli_run, user_tokens, vcr
     res = cli_run('analysis get --page 2')
     assert res.exit_code == 0
     assert len(json.loads(res.output)) == 0
-
     cli_run(f'analysis delete -p {draft_pid_one}')
     cli_run(f'analysis delete -p {draft_pid_two}')
 
@@ -73,7 +73,9 @@ def test_analysis_get_drafts_with_page_with_results(cli_run, user_tokens, vcr_co
 
     # create draft analyses
     create_draft_res_one = cli_run('analysis create --json {} --type alice-analysis')
+    time.sleep(2)
     create_draft_res_two = cli_run('analysis create --json {} --type cms-analysis')
+    time.sleep(2)
     draft_pid_one = json.loads(create_draft_res_one.output).get('pid')
     draft_pid_two = json.loads(create_draft_res_two.output).get('pid')
 
@@ -81,7 +83,6 @@ def test_analysis_get_drafts_with_page_with_results(cli_run, user_tokens, vcr_co
     res = cli_run('analysis get --size 1 --page 2')
     assert res.exit_code == 0
     assert len(json.loads(res.output)) >= 1
-
     cli_run(f'analysis delete -p {draft_pid_one}')
     cli_run(f'analysis delete -p {draft_pid_two}')
 
@@ -101,7 +102,6 @@ def test_analysis_get_drafts_with_size(cli_run, user_tokens, vcr_config):
     res = cli_run('analysis get --size 1')
     assert res.exit_code == 0
     assert len(json.loads(res.output)) == 1
-
     cli_run(f'analysis delete -p {draft_pid_one}')
     cli_run(f'analysis delete -p {draft_pid_two}')
 
@@ -113,7 +113,9 @@ def test_analysis_get_drafts_with_sort(cli_run, user_tokens, vcr_config):
 
     # create draft analyses
     create_draft_res_one = cli_run('analysis create --json {} --type alice-analysis')
+    time.sleep(2)
     create_draft_res_two = cli_run('analysis create --json {} --type cms-analysis')
+    time.sleep(2)
     draft_pid_one = json.loads(create_draft_res_one.output).get('pid')
     draft_pid_two = json.loads(create_draft_res_two.output).get('pid')
 
@@ -121,6 +123,5 @@ def test_analysis_get_drafts_with_sort(cli_run, user_tokens, vcr_config):
     res = cli_run('analysis get --sort mostrecent')
     assert res.exit_code == 0
     assert json.loads(res.output)[0].get('pid') == draft_pid_two
-
     cli_run(f'analysis delete -p {draft_pid_one}')
     cli_run(f'analysis delete -p {draft_pid_two}')
